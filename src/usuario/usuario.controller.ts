edd7ce61accac31ebcc54cf8b00e919de3791444
oscar <oscarmancilla227@gmail.com>
2024-09-25 14:20:47 -0600

    microservicio usuarios terminado

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Put } from '@nestjs/common';
 import { UsuarioService } from './usuario.service';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
@@ -7,15 +7,44 @@ import { UpdateUsuarioDto } from './dto/update-usuario.dto';
 export class UsuarioController {
   constructor(private readonly usuarioService: UsuarioService) {}
 
-  @Post()
-  create(@Body() createUsuarioDto: CreateUsuarioDto) {
-    return this.usuarioService.create(createUsuarioDto);
+  @Post('/crearUsuario')
+  async create(@Body() createUsuarioDto: CreateUsuarioDto) {
+    
+    try{
+
+      return await this.usuarioService.create(createUsuarioDto); 
+
+    }catch(error){
+      console.log(error);
+      return error;
+    }
+
   }
 
-  @Get()
-  findAll() {
-    return this.usuarioService.findAll();
+  @Get('/obtenerUsuarios')
+  async findAll() {
+
+    try{
+      return await this.usuarioService.findAll();
+    }catch(error){
+      console.log(error);
+      return error;
+    }
+
   }
+
+  @Put('/cambiarContrasenia/:id')
+  async cambiarContrasenia(@Param('id') id: string, @Body() updateUsuarioDto: UpdateUsuarioDto) {
+    try{
+
+      return await this.usuarioService.cambiarContrasenia(+id, updateUsuarioDto);
+    }catch(error){
+      console.log(error);
+      return error;
+    }
+
+  }
+
 
   @Get(':id')
   findOne(@Param('id') id: string) {
--- src/usuario/usuario.service.ts
+++ src/usuario/usuario.service.ts
@@ -1,15 +1,72 @@
 import { Injectable } from '@nestjs/common';
+import * as bcryptjs from 'bcryptjs';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
+import { InjectModel } from '@nestjs/sequelize';
+import { Usuario } from './entities/usuario.entity';
 
 @Injectable()
 export class UsuarioService {
-  create(createUsuarioDto: CreateUsuarioDto) {
-    return 'This action adds a new usuario';
+
+  constructor(@InjectModel(Usuario) private usuario: typeof Usuario){}
+
+
+  async create(createUsuarioDto: CreateUsuarioDto) {
+  
+    try{
+
+      let {nombre, contrasenia, email, rol} = createUsuarioDto;
+      let salt = await bcryptjs.genSaltSync(10)
+      let password = await bcryptjs.hashSync(contrasenia, salt);
+
+      let user = await this.usuario.create({nombre: nombre, contrasenia: password, email: email, rol: rol});
+
+      return {message: 'Usuario creado correctamente'};
+
+    }catch(error){
+      console.log(error);
+      return {message: 'Error al crear el usuario'};
+    }
+
+
   }
 
-  findAll() {
-    return `This action returns all usuario`;
+  async findAll() {
+    
+    try{
+
+      const users = await this.usuario.findAll();
+
+      const users_cleaned = users.map(user => {
+        return {nombre: user.nombre, email: user.email, rol: user.rol, estatus: user.estatus};
+      });
+
+      return users_cleaned;
+
+    }catch(error){
+      console.log(error);
+      return {message: 'Error al obtener los usuarios'};
+    }
+
+  }
+
+  async cambiarContrasenia(id: number, updateUsuarioDto: UpdateUsuarioDto) {
+
+    try{
+
+      let salt = await bcryptjs.genSaltSync(10);
+      let password = await bcryptjs.hashSync(updateUsuarioDto.contrasenia, salt);
+
+      const user = await this.usuario.findByPk(id);
+      await user.update({contrasenia: password});
+
+      return {message: 'Contraseña cambiada correctamente'};
+
+      
+    }catch(error){
+      console.log(error);
+      return {message: 'Error al cambiar la contraseña'};
+    }
   }
 
   findOne(id: number) {
