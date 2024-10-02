b5ec3ea87e928ce6ab5061e68b963aab0c168ff8
oscar <oscarmancilla227@gmail.com>
2024-10-02 12:20:45 -0600

    diferente config de cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*', // Permite todos los orígenes
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+    allowedHeaders: '*', // Permite todos los headers
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3001);
 }
